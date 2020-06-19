e5aa934ff516c79c379545045f0de7ae3268dc68
William Lee <wlee221@live.unc.edu>
2020-06-18 20:04:05 -0700

    feat: handle invalid entry into  analysis page

--- src/Pages/Analyze.tsx
+++ src/Pages/Analyze.tsx
@@ -8,16 +8,19 @@ type PropType = RouteChildrenProps<{}, { file: File }>;
 export const Analyze: React.FC<PropType> = (props: PropType) => {
     const [audio, setAudio] = useState<Float32Array>();
     const history = useHistory();
-    const file = props.location.state.file;
-
-    if (!file) {
-        history.push('/');
-    }
 
     const parseWav = () => {
         const reader = new FileReader();
         const audioContext = new AudioContext();
 
+        const propsIsUndefined = !props.location.state || !props.location.state.file;
+        if (propsIsUndefined && audioContext.state === 'suspended' && !audio) {
+            // user accessed this link through invalid flow (e.g. directly entered the link). Redirect user to home.
+            history.push('/');
+            return;
+        }
+
+        const file = props.location.state.file;
         const onFileLoad = (event: ProgressEvent<FileReader>) => {
             if (event.target.result) {
                 audioContext.decodeAudioData(reader.result as ArrayBuffer).then(buffer => {
@@ -32,21 +35,20 @@ export const Analyze: React.FC<PropType> = (props: PropType) => {
                     const playEnd = audio.length - [...audio].reverse().findIndex(val => val >= threshold);
 
                     setAudio(audio.slice(playStart, playEnd));
-                });
+                }).catch(e => console.error(e));
             }
         };
-
         reader.onload = onFileLoad;
         reader.readAsArrayBuffer(file);
     };
     useEffect(parseWav, []); // run when it is mounted // TODO: test going back home then returning with new file
 
-    if (!audio) return <div>Processing audio...</div>;
-
     const back = () => {
         console.log(history);
         history.goBack();
     };
+
+    if (!audio) return <div>Processing audio...</div>;
 
     return <div>
         <Graph array={audio} windowSize={1000} title="Waveform" />
